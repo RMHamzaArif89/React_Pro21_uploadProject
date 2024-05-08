d6f5400422c509b229ebe21ad7655151534451f1
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-05-08 09:04:46 +0500

    crated the projects components & css file

--- Project_Creator/src/App.jsx
+++ Project_Creator/src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 import Sidebar from './components/Sidebar'
 import Blank from './components/blank'
+import Projects from './components/projects'
 
 import './App.css'
 
 function App() {
+  let[con,setCon]=useState(false)
 
   return (
   <>
 <div className="container">
 <Sidebar/>
-  <Blank/>
+  {
+   con?<Blank/>:<Projects/>
+  }
 </div>
   </>
   )
---
+++ Project_Creator/src/components/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './css/projects.css'
+
+function Projects() {
+  return (
+    <div className='projects'>
+        <div className="name">Project: </div>
+        <div className="description">description</div>
+        <div className="date">date</div>
+      
+    </div>
+  )
+}
+
+export default Projects
